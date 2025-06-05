432439a9fbe0e6f8084b38dd8d9041af86fa8f74
Samuel <samuelrichardrendl@gmail.com>
2025-06-05 17:01:36 +0200

    .

--- components/matchcard/MatchCard.tsx
+++ components/matchcard/MatchCard.tsx
@@ -116,7 +116,7 @@ const MatchCard = async ({
                 <div className="flex items-center justify-center gap-2 text-xs sm:flex-col sm:gap-0.5">
                   <p className="font-bold leading-none">{gameType}</p>
                   <p className="">
-                    {user?.placement! > 0 ? `${user?.placement!}th` : ""}
+                    {user && user.placement !== undefined && user.placement > 0 ? `${user.placement}th` : ""}
                   </p>
                   <p className="text-[10px] font-light leading-none">
                     {howLongAgo}
