53fe2735b029b6240b6edd3adbd3e144137f18ac
Samuel Rendl <samuelrichardrendl@gmail.com>
2025-07-01 20:08:06 +0200

    add regions select to searchbar

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -5,15 +5,15 @@ import { Suspense } from "react";
 const Page = async ({
   searchParams,
 }: {
-  searchParams: Promise<{ username?: string }>;
+  searchParams: Promise<{ username: string, region: string }>;
 }) => {
-  const { username } = await searchParams;
+  const { username, region } = await searchParams;
   if (!username) return <div>No username provided.</div>;
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Navbar />
-      <MatchList username={username} />
+      <MatchList username={username} region={region} />
     </Suspense>
   );
 };
